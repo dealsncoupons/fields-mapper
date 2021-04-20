ee506199c49318ec4b24ce07549deb91843c7d6f
Maina <925723@cognizant.com>
2021-04-20 12:53:13 -0500

    some little cleanup

--- src/main/java/works/hop/fields/mapper/config/MigrationConfig.java
+++ src/main/java/works/hop/fields/mapper/config/MigrationConfig.java
@@ -1,6 +1,7 @@
 package works.hop.fields.mapper.config;
 
 //import org.flywaydb.core.Flyway;
+import org.flywaydb.core.Flyway;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,13 +12,13 @@ import org.springframework.context.annotation.PropertySource;
 @PropertySource("classpath:application-local.properties")
 public class MigrationConfig {
 
-//    @Bean
-//    Flyway flyway(DbProperties properties) {
-//        Flyway flyway = Flyway.configure()
-//                .dataSource(properties.url, properties.username, properties.password)
-//                .baselineOnMigrate(true)
-//                .load();
-//        flyway.migrate();
-//        return flyway;
-//    }
+    @Bean
+    Flyway flyway(DbProperties properties) {
+        Flyway flyway = Flyway.configure()
+                .dataSource(properties.url, properties.username, properties.password)
+                .baselineOnMigrate(true)
+                .load();
+        flyway.migrate();
+        return flyway;
+    }
 }
